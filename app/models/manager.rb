321b1a3a07c489f37fca8723c62be44b01d187e3
ihowardos <asergey96@gmail.com>
2018-03-18 23:46:31 +0300

    Quality fix

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,4 +1,4 @@
 class Manager < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable,
+    :rememberable, :trackable, :validatable
 end
