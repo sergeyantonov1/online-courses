2b28283f4ab946a9b5ff0972ea1f04e58fa0b87c
ihowardos <asergey96@gmail.com>
2018-03-25 16:58:40 +0300

    Add FactoryGirl dependency

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -1,3 +1,5 @@
+require 'factory_girl_rails'
+
 FactoryGirl.create(:user, email: "user@example.com")
 FactoryGirl.create(:manager, email: "super_manager@example.com", super_manager: true)
 FactoryGirl.create(:manager, email: "manager@example.com")
