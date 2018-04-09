3eb368fd3d592c80e433246166c8ce533529e29a
latypova-alina <alinkin9696@mail.ru>
2018-04-09 12:58:01 +0300

    add model test and factory teacher

---
+++ spec/factories/teachers.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :teacher do
+    first_name
+    second_name
+    email
+    password "password"
+  end
+end
---
+++ spec/models/teacher_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Teacher do
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_presence_of :second_name }
+end
