0a3b553a8df6084d198d83794bd524943c4ba302
latypova-alina <alinkin9696@mail.ru>
2018-04-09 20:06:00 +0300

    add teachers seeds

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -3,3 +3,4 @@ require 'factory_girl_rails'
 FactoryGirl.create(:user, email: "user@example.com")
 FactoryGirl.create(:manager, email: "super_manager@example.com", super_manager: true)
 FactoryGirl.create(:manager, email: "manager@example.com")
+FactoryGirl.create(:teacher, email: "teacher@example.com")
