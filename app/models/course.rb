457dc6b324602a334cd1dcd471744bd9fde06c72
latypova-alina <alinkin9696@mail.ru>
2018-04-18 03:41:20 +0300

    minor fixes for course files

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-  mount_uploader :course_file, CourseFileUploader
+  mount_uploaders :course_file, CourseFileUploader
   belongs_to :teacher
 
   validates :title, :description, :teacher, presence: true
