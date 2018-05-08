71712aba179e2d4742f90b3ddf9a768e98cc43f0
Sergey Antonov <33006817+sergeyantonov1@users.noreply.github.com>
2018-05-08 17:49:29 +0300

    Fix bug subscription (#11)
    * Change structure of controllers and fix bug with query object
    
    * Add application to the manager from client (student)

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -14,14 +14,14 @@ class UserDecorator < ApplicationDecorator
   end
 
   def subscription_approved?(id)
-    object.subscriptions.find_by(course_id: id).status == "approved"
+    object.subscriptions.find_by(course_id: id)&.status == "approved"
   end
 
   def subscription_canceled?(id)
-    object.subscriptions.find_by(course_id: id).status == "canceled"
+    object.subscriptions.find_by(course_id: id)&.status == "canceled"
   end
 
   def subscription_new?(id)
-    object.subscriptions.find_by(course_id: id).status == "new"
+    object.subscriptions.find_by(course_id: id)&.status == "new"
   end
 end
