ae771896a2b36f0efb81a2d5f8bbee71d04c9539
latypova-alina <alinkin9696@mail.ru>
2018-05-19 23:22:52 +0300

    some controller changes

--- app/controllers/teachers/tasks_controller.rb
+++ app/controllers/teachers/tasks_controller.rb
@@ -3,25 +3,22 @@ module Teachers
     expose :lesson, -> { Lesson.find(params[:lesson_id]) }
     expose :course, -> { lesson.course }
     expose :tasks, -> { lesson.tasks }
-    expose :task, -> { Task.find(params[:id]) }
-
-    def new
-      @task = Task.new
-    end
+    expose :task
 
     def create
-      @task = Task.new(task_params) do |task|
-        task.lesson_id = params[:lesson_id]
-      end
-      if @task.save
-        redirect_to teachers_course_lesson_path(lesson.course, lesson)
-      else
-        render :new
-      end
+      task.lesson = lesson
+      task.save
+
+      redirect_to teachers_course_lesson_path(lesson.course, lesson)
     end
 
     def update
       task.update(task_params)
+      redirect_to teachers_course_lesson_tasks_path(lesson.course, lesson)
+    end
+
+    def destroy
+      task.destroy
       redirect_to teachers_course_lesson_tasks_path(lesson.course, lesson)
     end
 
